c5e37390200d64a0158a8644d6c634d9bc2bc2cf
noahlibeskind <noahlibeskind@gmail.com>
2022-12-17 15:41:19 -0800

    displaying JSON correctly on getChannels, need to fix elsewhere

--- notslack.go
+++ notslack.go
@@ -5,8 +5,9 @@ import (
 	// "log"
 	"net/http"
 	"os/exec"
+	"strconv"
 
-	// "time"
+	"time"
 
 	"github.com/noahlibeskind/NotSlackAPI/utils"
 
@@ -30,16 +31,16 @@ type workspace struct {
 }
 
 type channel struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	Messages    string `json:"messages"`
-	WorkspaceID string
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Messages string `json:"messages"`
 }
 
 type message struct {
-	ID     string `json:"id"`
-	Poster string `json:"poster"`
-	Posted string `json:"posted"`
+	ID      string    `json:"id"`
+	Content string    `json:"content"`
+	Poster  string    `json:"poster"`
+	Posted  time.Time `json:"posted"`
 }
 
 // mapping of workspace ids to the users inside them
@@ -50,13 +51,37 @@ var users = []user{
 }
 
 var workspaces = []workspace{
-	{ID: "00000000-0000-0000-0000-000000000000", Name: "(WWW) World Wide Workspace", Channels: "1"},
+	{ID: "00000000-0000-0000-0000-000000000000", Name: "(WWW) World Wide Workspace", Channels: "0"},
 }
 
+var channels = []channel{
+	{ID: "00000000-0000-0000-0000-000000000000", Name: "World Chat Channel", Messages: "0"},
+}
+
+// maps workspace IDs to IDs of users in that workspace
 var workspace_users = map[string][]string{}
 
-var mySigningKey = []byte("mysecretphrase")
+// maps workspace IDs to IDs of channels in that workspace
+var workspace_channels = map[string][]string{}
 
+// maps channel IDs to IDs of messages in that channel
+var channel_messages = map[string][]string{}
+
+var mySigningKey = []byte("notslackisnotsecure")
+
+// https://play.golang.org/p/Qg_uv_inCek
+// contains checks if a string is present in a slice
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+
+// get all users
 func getUsers(context *gin.Context) {
 	tokenStatus, err := utils.ExtractTokenID(context)
 
@@ -67,6 +92,8 @@ func getUsers(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, users)
 }
 
+// get all workspaces
+// todo: only get ws associated with a user from accessToken
 func getWorkSpaces(context *gin.Context) {
 	tokenStatus, err := utils.ExtractTokenID(context)
 
@@ -77,6 +104,7 @@ func getWorkSpaces(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, workspaces)
 }
 
+// creates a new workspace with logged in user as the owner
 func createWorkSpace(context *gin.Context) {
 	var newWS workspace
 
@@ -107,6 +135,8 @@ func createWorkSpace(context *gin.Context) {
 	return
 }
 
+// adds a member with id memId to workspace with id wsId
+// todo: make sure owner of wsId == loggedInUser
 func addWorkSpaceMember(context *gin.Context) {
 	// workspace id
 	wsId := context.Param("wsId")
@@ -135,6 +165,7 @@ func addWorkSpaceMember(context *gin.Context) {
 	}
 }
 
+// gets all users in the workspace with specified id
 func workSpaceMembers(context *gin.Context) {
 	// workspace id
 	id := context.Param("id")
@@ -158,15 +189,145 @@ func workSpaceMembers(context *gin.Context) {
 	return
 }
 
-// func getTodoByID(id string) (*todo, error) {
-// 	for i, t := range todos {
-// 		if t.ID == id {
-// 			return &todos[i], nil
-// 		}
-// 	}
+// adds a member with id memId to workspace with id wsId
+// todo: make sure owner of wsId == loggedInUser
+func createChannel(context *gin.Context) {
+	// workspace id
+	id := context.Param("id")
 
-//		return nil, errors.New("not found")
-//	}
+	var newChannel channel
+	// JSON should include channel name only
+	err := context.BindJSON(&newChannel)
+
+	tokenStatus, err := utils.ExtractTokenID(context)
+	// verify auth
+	if err != nil || tokenStatus == 0 {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	} else {
+		for i, t := range workspaces {
+			if t.ID == id {
+				// found a workspace with this id, add channel to it
+				newUUID, err := exec.Command("uuidgen").Output()
+				if err != nil {
+					return
+				}
+				newChannel.ID = string(newUUID)[0 : len(newUUID)-1]
+
+				// add id to map
+				_, ok := workspace_channels[id]
+				if ok {
+					workspace_channels[id] = append(workspace_channels[id], newChannel.ID)
+				} else {
+					workspace_channels[id] = []string{newChannel.ID}
+				}
+				newChannel.Messages = "0"
+				count, _ := strconv.Atoi(workspaces[i].Channels)
+				workspaces[i].Channels = strconv.Itoa(count + 1)
+				channels = append(channels, newChannel)
+				context.IndentedJSON(http.StatusOK, workspace_channels[id])
+				return
+			}
+		}
+	}
+}
+
+func getChannels(context *gin.Context) {
+	tokenStatus, err := utils.ExtractTokenID(context)
+
+	// workspace id
+	id := context.Param("id")
+
+	if err != nil || tokenStatus == 0 {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var wsChannels = []channel{}
+	for _, t := range channels {
+		if contains(workspace_channels[id], t.ID) {
+			wsChannels = append(wsChannels, t)
+		}
+	}
+	context.IndentedJSON(http.StatusOK, wsChannels)
+}
+
+// adds a member with id memId to workspace with id wsId
+// todo: make sure owner of wsId == loggedInUser
+func createMessage(context *gin.Context) {
+	// channel id
+	id := context.Param("id")
+
+	var newMessage message
+	// JSON should include message content only
+	err := context.BindJSON(&newMessage)
+
+	tokenStatus, err := utils.ExtractTokenID(context)
+	token := utils.ExtractToken(context)
+
+	// verify auth
+	if err != nil || tokenStatus == 0 {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	} else {
+		for _, t := range users {
+			if t.AccessToken == token {
+				// get ID from AccessToken
+				newMessage.Poster = t.ID
+			}
+		}
+		for i, t := range channels {
+			if t.ID == id {
+				// found a channel with this id, add message to it
+				newUUID, err := exec.Command("uuidgen").Output()
+				if err != nil {
+					return
+				}
+				newMessage.ID = string(newUUID)[0 : len(newUUID)-1]
+
+				// add id to map
+				_, ok := channel_messages[id]
+				if ok {
+					channel_messages[id] = append(channel_messages[id], newMessage.ID)
+				} else {
+					channel_messages[id] = []string{newMessage.ID}
+				}
+				// get current time
+				newMessage.Posted = time.Now()
+				count, _ := strconv.Atoi(channels[i].Messages)
+				channels[i].Messages = strconv.Itoa(count + 1)
+				context.IndentedJSON(http.StatusOK, channel_messages[id])
+				return
+			}
+		}
+	}
+}
+
+// adds a member with id memId to workspace with id wsId
+// todo: make sure owner of wsId == loggedInUser
+func getMessages(context *gin.Context) {
+	// channel id
+	id := context.Param("id")
+	tokenStatus, err := utils.ExtractTokenID(context)
+
+	// verify auth
+	if err != nil || tokenStatus == 0 {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	} else {
+		for _, t := range channels {
+			if t.ID == id {
+				// found a channel with this id, add message to it
+				context.IndentedJSON(http.StatusOK, channel_messages[id])
+				return
+			}
+		}
+	}
+	context.JSON(http.StatusBadRequest, gin.H{"message": "channel not found"})
+	return
+}
+
+// creates a new user
 func createUser(context *gin.Context) {
 	var newUser user
 
@@ -195,6 +356,7 @@ func createUser(context *gin.Context) {
 	}
 }
 
+// logs in user with specified email and password, giving them a JWT
 func login(context *gin.Context) {
 	var loginUser user
 
@@ -250,28 +412,21 @@ func login(context *gin.Context) {
 // }
 
 func main() {
-	// newUUID, err := exec.Command("uuidgen").Output()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println("Generated UUID:")
-	// fmt.Printf("%s", newUUID)
-
 	router := gin.Default()
 	router.POST("/login", login)
 	router.POST("/newuser", createUser)
-
 	router.GET("/member", getUsers)
-	router.GET("/workspace", getWorkSpaces)
 
+	router.GET("/workspace", getWorkSpaces)
 	router.POST("/workspace", createWorkSpace)
 	router.GET("/workspace/:id/member", workSpaceMembers)
 	router.POST("/workspace/:wsId/member/:memId", addWorkSpaceMember)
-	// router.GET("/todos/:id", getTodo)
-	// // Patch allows editing existing entries
-	// router.PATCH("/todos/:id", toggleTodoStatus)
 
-	// router.DELETE("/todos/:id", deleteTodo)
-	// router.POST("/todos", addTodo)
+	router.POST("/workspace/channel/:id", createChannel)
+	router.GET("/workspace/channel/:id", getChannels)
+
+	router.POST("/channel/:id/message", createMessage)
+	router.GET("/channel/:id/message", getMessages)
+
 	router.Run("localhost:9090")
 }
